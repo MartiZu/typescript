4661ed8b5cad8928f1a6da0f24c851a1abcf7ecc
MartiZu <martina.zurli@gmail.com>
2024-01-30 23:32:15 +0000

    set data

--- shopping-typescript/cart-project/src/context/ProductsProvider.tsx
+++ shopping-typescript/cart-project/src/context/ProductsProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, createContext, useState } from "react";
+import { ReactElement, createContext, useState, useEffect } from "react";
 
 export type ProductType = {
   sku: string;
@@ -6,24 +6,25 @@ export type ProductType = {
   price: number;
 };
 
-const initState: ProductType[] = [
-  {
-    sku: "item001",
-    name: "Widget",
-    price: 10.0,
-  },
-  {
-    sku: "item002",
-    name: "Gadget",
-    price: 20.0,
-  },
-  {
-    sku: "item003",
-    name: "Gizmo",
-    price: 30.0,
-  },
-];
+// const initState: ProductType[] = [
+//   {
+//     sku: "item001",
+//     name: "Widget",
+//     price: 10.0,
+//   },
+//   {
+//     sku: "item002",
+//     name: "Gadget",
+//     price: 20.0,
+//   },
+//   {
+//     sku: "item003",
+//     name: "Gizmo",
+//     price: 30.0,
+//   },
+// ];
 
+const initState: ProductType[] = [];
 export type UseProductsContextType = { products: ProductType[] };
 
 const initContextState: UseProductsContextType = { products: [] };
@@ -34,4 +35,26 @@ type ChildrenType = { children?: ReactElement | ReactElement[] };
 
 export const ProductsProviders = ({ children }: ChildrenType): ReactElement => {
   const [products, setProducts] = useState<ProductType[]>(initState);
+
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      const data = await fetch("https://localhost:3500/products")
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          if (err instanceof Error) console.log(err.message);
+        });
+        return data;
+    };
+    fetchProducts().then(products => setProducts=(products))
+  }, []);
+
+  return (
+    <ProductsContext.Provider value={{ products }}>
+      {children}
+    </ProductsContext.Provider>
+  );
 };
+
+export default ProductsContext;
